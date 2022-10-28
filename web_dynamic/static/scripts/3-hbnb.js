8e30d898f0fc238d8e87625f913eb4f259013b93
Nabil <nabil.noble01@gmail.com>
2022-10-28 12:53:36 +0000

    complete task 4

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,55 @@
+$(document).ready( function () {
+    const checkedAmenities = {};
+    $('input[type=checkbox]').change(function () {
+      if ($(this).prop('checked')) {
+        checkedAmenities[$(this).attr('data-id')] = $(this).attr('data-name');
+      } else if (!$(this).prop('checked')) {
+        delete checkedAmenities[$(this).attr('data-id')];
+      }
+      if (Object.keys(checkedAmenities).length === 0) {
+        $('div.amenities h4').html('&nbsp');
+      } else {
+        $('div.amenities h4').text(Object.values(checkedAmenities).join(', '));
+      }
+    });
+
+    const api_url = `http://0.0.0.0:5001/api/v1/status/`;
+    $.get(api_url, function (data, textStatus) {
+      if (textStatus === 'success'){
+        if (data.status === 'OK') {
+          $('#api_status').addClass('available');
+        } else {
+          $('#api_status').removeClass('available');
+        } 
+      }
+    });
+
+    const places_search_url = `http://0.0.0.0:5001/api/v1/places_search/`;
+    $.ajax({
+      url: places_search_url,
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({}),
+      success: function (data) {
+        for (const d in data) {
+          //creating an article
+          const article = `<article>
+            <div class="title_box">
+              <h2>${d.name}</h2>
+              <div class="price_by_night">$${d.price_by_night}</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">${d.max_guest} Guest(s)</div>
+              <div class="number_rooms">${d.number_rooms} Bedroom(s)</div>
+              <div class="number_bathrooms">${d.number_bathrooms} Bathroom(s)</div>
+            </div>
+            <div class="description">
+              ${d.description}
+            </div>
+        </article>`
+          //appending the article to the section
+          $('section.places').append(article);
+        }
+      }
+    });
+});
